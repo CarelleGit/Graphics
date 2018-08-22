5703d10f9d6c08aa123249a3b66884a8b5b05464
Carelle <steamtik@gmail.com>
2018-08-21 17:54:24 -0700

    Fixed up Animation, Added Rain Particels

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -10,17 +10,10 @@ public class Controller : MonoBehaviour
     public float currentSpeed = 10;
     public float maxSpeed = 10;
     public float minSpeed = 3;
-
-    [SerializeField]
-    private float currentBlend = 0.0f;
-    private float maxBlending = 1.0f;
-    private float minBlending = 0.0f;
-
     // Use this for initialization
     void Start()
     {
         currentSpeed = minSpeed;
-        playerAnim.SetTrigger("Walking");
     }
 
     // Update is called once per frame
@@ -32,49 +25,36 @@ public class Controller : MonoBehaviour
         transform.Rotate(0, x, 0);
         transform.Translate(0, 0, z);
 
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))
+        playerAnim.SetFloat("WalkToRun", currentSpeed);
+        if (currentSpeed <= minSpeed)
         {
-            //if(currentBlend >= minBlending)
-            //{
-            //    currentBlend -= .2f;
-            //}
-            if (currentBlend >= minBlending && currentBlend <= maxBlending)
-            {
-                
-                currentBlend += .2f;
-                playerAnim.SetFloat("WalkToRun", currentBlend);
-            }
-            if (currentBlend >= 0.5f && !Input.GetKey(KeyCode.LeftShift))
-            {
-                currentBlend = 0.335f;
-                playerAnim.SetFloat("WalkToRun", currentBlend);
-            }
-
+            currentSpeed = minSpeed;
         }
-        else
+        if (currentSpeed >= maxSpeed)
         {
-            if (currentBlend >= minBlending)
-            {
-                currentBlend -= .02f;
-            }
-            //if (currentBlend <= minBlending)
-            //{
-            currentSpeed = 0;
-            //playerAnim.SetFloat("WalkToRun", minBlending);
-            //}
+            currentSpeed = maxSpeed;
+        }
 
+        if (Input.GetKey(KeyCode.W))
+        {
+            if (currentSpeed >= 2 && !Input.GetKey(KeyCode.LeftShift))
+            {
+                //currentSpeed = maxSpeed / 2;
+                currentSpeed -= Time.deltaTime * 5;
+            }
+            else
+            {
+                currentSpeed += Time.deltaTime * 10;
+            }
 
         }
         if (Input.GetKey(KeyCode.LeftShift))
         {
-            currentBlend += .02f;
-            currentSpeed += .02f;
-            playerAnim.SetFloat("WalkToRun", currentBlend);
-            //if (currentSpeed >= maxSpeed || currentBlend >= maxBlending)
-            //{
-                currentSpeed = maxSpeed;
-                currentBlend = maxBlending;
-            //}
+            currentSpeed += Time.deltaTime * 10;
+        }
+        else
+        {
+            currentSpeed -= Time.deltaTime * 5;
         }
     }
 }
