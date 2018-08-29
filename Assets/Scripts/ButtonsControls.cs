8340f45f770d2e99a587eb2aa26f20a96b31bf2f
Carelle <steamtik@gmail.com>
2018-08-29 12:46:01 -0700

    Added UI and intracts with Particles

---
+++ Assets/Scripts/ButtonsControls.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonsControls : MonoBehaviour
+{
+    [SerializeField]
+    private RectTransform controlPanel;
+    [SerializeField]
+    private bool placeholder;
+
+    public void OpenPanel()
+    {
+        placeholder = true;
+    }
+
+    public void ClosePanel()
+    {
+        placeholder = false;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        
+        placeholder = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(placeholder == false)
+        {
+            controlPanel.position = Vector3.Lerp(controlPanel.position, new Vector3(0, controlPanel.position.y), Time.deltaTime * 10);
+        }
+        else
+        {
+            controlPanel.position = Vector3.Lerp(controlPanel.position, new Vector3(Screen.width, controlPanel.position.y), Time.deltaTime * 10);
+        }
+    }
+}
---
+++ Assets/Scripts/ParticleController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ParticleController : MonoBehaviour
+{
+
+    public Slider rainDensitySlider;
+    public void StopParticls(ParticleSystem rain)
+    {
+        if (rain.isStopped)
+        {
+            rain.Play();
+        }
+        else
+        {
+            rain.Stop();
+        }
+    }
+
+    public void ParticleDensity(ParticleSystem rain)
+    {
+        var emission = rain.emission;
+        emission.rateOverTime = rainDensitySlider.value;
+    }
+}
