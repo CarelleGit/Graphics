7980a40953220498e3b6081ccab89eb1fdc5c4ae
Carelle <steamtik@gmail.com>
2018-08-28 17:39:29 -0700

    Made the rain better by adding sub-emitters

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -8,66 +8,128 @@ public class Controller : MonoBehaviour
     private Animator playerAnim;
 
     public float currentSpeed = 10;
+
+
+
     public float maxSpeed = 10;
     public float minSpeed = 3;
+    [SerializeField]
+    private Rigidbody rigidbody;
     // Use this for initialization
     void Start()
     {
         currentSpeed = minSpeed;
     }
 
+
+
+    float x = 0;
+    float z = 0;
+
+
     // Update is called once per frame
     void Update()
     {
+        //sets player speed to 0 if no input is detected
+        if ((Input.GetAxis("Horizontal") == 0) && (Input.GetAxis("Vertical") == 0))
+        {
+            x = 0;
+            z = 0;
+        }
 
-        float x = Input.GetAxis("Horizontal") * Time.deltaTime * currentSpeed;
-        float z = Input.GetAxis("Vertical") * Time.deltaTime * currentSpeed;
 
-        transform.Rotate(0, x, 0);
-        transform.Translate(0, 0, z);
+        //input to movement handler
+        Vector3 movement = new Vector3(x, 0.0f, z);
+
+        //moyion handler
+        rigidbody.MovePosition(transform.position + movement);
+
+
 
         playerAnim.SetFloat("WalkToRun", currentSpeed);
-        
 
+        //regular walking controller
         if (Input.GetKey(KeyCode.W))
         {
-            if (currentSpeed <= minSpeed)
-            {
-                currentSpeed = minSpeed;
-            }
-            if (currentSpeed >= maxSpeed)
-            {
-                currentSpeed = maxSpeed;
-            }
+            //applies speed & player input to character object for walking
+            x = Input.GetAxis("Horizontal") * Time.deltaTime * currentSpeed;
+            z = Input.GetAxis("Vertical") * Time.deltaTime * currentSpeed;
+
+
             if (currentSpeed >= 2 && !Input.GetKey(KeyCode.LeftShift))
             {
-                //currentSpeed = maxSpeed / 2;
+                //regulates walking speed
+                if (currentSpeed <= minSpeed)
+                {
+                    currentSpeed = minSpeed;
+                }
+                if (currentSpeed >= maxSpeed)
+                {
+                    currentSpeed = maxSpeed;
+                }
+
                 currentSpeed -= Time.deltaTime * 5;
             }
             else
             {
                 currentSpeed += Time.deltaTime * 10;
+
+
+                //regulates walking speed
+                if (currentSpeed <= minSpeed * 2)
+                {
+                    currentSpeed = minSpeed * 2;
+                }
+                if (currentSpeed >= maxSpeed * 2)
+                {
+                    currentSpeed = maxSpeed * 2;
+                }
+
             }
 
         }
-        if(Input.GetKey(KeyCode.S))
-        { 
+
+
+        //backwards walking controller
+        if (Input.GetKey(KeyCode.S))
+        {
+            //applies speed & player input to character object for walking in reverse
+            x = Input.GetAxis("Horizontal") * Time.deltaTime * -currentSpeed;
+            z = Input.GetAxis("Vertical") * Time.deltaTime * -currentSpeed;
+
             playerAnim.SetTrigger("WalkingBack");
-           
+
+
+            //regulates reverse speed
+            if (currentSpeed <= -minSpeed)
+            {
+                currentSpeed = -minSpeed;
+            }
+            if (currentSpeed >= -maxSpeed)
+            {
+                currentSpeed = -maxSpeed;
+            }
+            if (currentSpeed >= 2 && !Input.GetKey(KeyCode.LeftShift))
+            {
+
+                currentSpeed -= -Time.deltaTime * 5;
+            }
+            else
+            {
+                currentSpeed += -Time.deltaTime * 10;
+            }
+
         }
         else
         {
             playerAnim.SetTrigger("Walking");
 
+
         }
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            currentSpeed += Time.deltaTime * 10;
-        }
-        else
-        {
-            currentSpeed -= Time.deltaTime * 5;
-        }
-       
+
+        //
+        currentSpeed -= Time.deltaTime * 5;
+
+
     }
 }
